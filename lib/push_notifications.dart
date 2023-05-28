df0365c9db42065b171ac272c65405144521497b
Esaias Cronelius <esaias@cronelius.se>
2023-05-28 20:56:12 +0200

    Only show new notifications

--- lib/push_notifications.dart
+++ lib/push_notifications.dart
@@ -3,23 +3,74 @@ import 'dart:convert';
 import 'package:flutter_local_notifications_test/http.dart';
 import 'package:flutter_local_notifications_test/notification.dart';
 import 'package:flutter_local_notifications_test/notification_service.dart';
+import 'package:flutter_local_notifications_test/storage.dart';
 
+/// Fetches notifications from the server and displays new ones.
 Future<void> fetchNotifications() async {
   List<Notification> notifications = await _getNotifications();
+  notifications.sort((a, b) => a.id.compareTo(b.id));
 
   if (notifications.isNotEmpty) {
-    NotificationService notificationService = NotificationService();
-    await notificationService.initializePlatformNotifications();
+    String? lastShownNotificationIdStr = await _getLastShownNotificationId();
 
-    for (final notification in notifications) {
-      await notificationService.showLocalNotification(
-        id: notification.id,
-        title: notification.title,
-        body: notification.body,
-        payload: notification.payload,
-      );
+    // First time running the app.
+    if (lastShownNotificationIdStr == null) {
+      return await _setLastShownNotificationId(notifications);
+    }
+
+    // Display notifications that haven't been shown yet.
+    int lastShownNotificationId = int.parse(lastShownNotificationIdStr);
+    await _showNotifications(notifications, lastShownNotificationId);
+
+    // Update the last shown notification id.
+    await _setLastShownNotificationId(notifications);
+  }
+}
+
+/// Displays a notification.
+Future<void> _showNotification(
+    NotificationService notificationService, Notification notification) async {
+  await notificationService.showLocalNotification(
+    id: notification.id,
+    title: notification.title,
+    body: notification.body,
+    payload: notification.payload,
+  );
+}
+
+/// Displays notifications that haven't been shown yet.
+Future<void> _showNotifications(
+    List<Notification> notifications, int lastShownNotificationId) async {
+  NotificationService notificationService =
+      await _initializeNotificationService();
+
+  for (final notification in notifications) {
+    if (notification.id > lastShownNotificationId) {
+      await _showNotification(notificationService, notification);
     }
   }
+}
+
+/// Sets the last shown notification id in storage.
+Future<void> _setLastShownNotificationId(
+    List<Notification> notifications) async {
+  int lastShownNotificationId = notifications.last.id;
+  await Storage.set(
+    'lastShownNotificationId',
+    lastShownNotificationId.toString(),
+  );
+}
+
+/// Gets the last shown notification id from storage.
+Future<String?> _getLastShownNotificationId() async {
+  return await Storage.get('lastShownNotificationId', throwOnFail: false);
+}
+
+/// Initializes a new instance of [NotificationService].
+Future<NotificationService> _initializeNotificationService() async {
+  NotificationService notificationService = NotificationService();
+  await notificationService.initializePlatformNotifications();
+  return notificationService;
 }
 
 /// Fetches notifications from the server.
